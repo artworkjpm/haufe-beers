bb5c15f7fb3eb8c7e81e3299fd652ed66e543b05
John Moran <johnnymoran44@hotmail.com>
2019-07-13 16:58:14 +0200

    routing to details page

--- src/app/beers/beer-list/beer-list.component.ts
+++ src/app/beers/beer-list/beer-list.component.ts
@@ -1,24 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { select, Store } from '@ngrx/store';
-import { DrinksState } from '../store';
-import { Observable } from 'rxjs/index';
-import { getBeersSelector } from '../store/beers.selectors';
-import { fetchBeersListRequest } from '../store/beers.actions';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { select, Store } from "@ngrx/store";
+import { DrinksState } from "../store";
+import { Observable } from "rxjs/index";
+import { getBeersSelector } from "../store/beers.selectors";
+import { fetchBeersListRequest } from "../store/beers.actions";
 
 @Component({
-  selector: 'app-beer-list',
-  templateUrl: './beer-list.component.html',
-  styleUrls: ['./beer-list.component.scss']
+  selector: "app-beer-list",
+  templateUrl: "./beer-list.component.html",
+  styleUrls: ["./beer-list.component.scss"]
 })
 export class BeerListComponent implements OnInit {
-
   public beers$: Observable<any>;
 
-  constructor(private store: Store<DrinksState>) { }
+  constructor(private store: Store<DrinksState>, private router: Router) {}
 
   ngOnInit() {
     this.store.dispatch(fetchBeersListRequest());
     this.beers$ = this.store.pipe(select(getBeersSelector));
   }
-
 }
