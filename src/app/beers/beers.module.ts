e62f1accc4e914a1e8d7892f4ea2bb50b6412ae0
John Moran <johnnymoran44@hotmail.com>
2019-07-13 16:47:57 +0200

    beer details

--- src/app/beers/beers.module.ts
+++ src/app/beers/beers.module.ts
@@ -1,28 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BeerListComponent } from './beer-list/beer-list.component';
-import { RouterModule } from '@angular/router';
-import { BeersRouting } from './beers.routing';
-import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
-import { beersReducers } from './store';
-import { EffectsModule } from '@ngrx/effects';
-import { BeersEffects } from './store/beers.effects';
-import { BeersService } from './beers.service';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BeerListComponent } from "./beer-list/beer-list.component";
+import { RouterModule } from "@angular/router";
+import { BeersRouting } from "./beers.routing";
+import { HttpClientModule } from "@angular/common/http";
+import { StoreModule } from "@ngrx/store";
+import { beersReducers } from "./store";
+import { EffectsModule } from "@ngrx/effects";
+import { BeersEffects } from "./store/beers.effects";
+import { BeersService } from "./beers.service";
+import { BeerDetailsComponent } from "./beer-details/beer-details.component";
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule,
     RouterModule.forChild(BeersRouting),
-    StoreModule.forFeature('drinks', beersReducers),
+    StoreModule.forFeature("drinks", beersReducers),
     EffectsModule.forFeature([BeersEffects])
   ],
-  declarations: [
-    BeerListComponent
-  ],
-  providers: [
-    BeersService
-  ]
+  declarations: [BeerListComponent, BeerDetailsComponent],
+  providers: [BeersService]
 })
-export class BeersModule { }
+export class BeersModule {}
--- src/app/beers/beers.routing.ts
+++ src/app/beers/beers.routing.ts
@@ -1,10 +1,14 @@
-import { Routes } from '@angular/router';
-import { BeerListComponent } from './beer-list/beer-list.component';
-
+import { Routes } from "@angular/router";
+import { BeerListComponent } from "./beer-list/beer-list.component";
+import { BeerDetailsComponent } from "./beer-details/beer-details.component";
 
 export const BeersRouting: Routes = [
   {
-    path: 'beers',
+    path: "beers",
     component: BeerListComponent
+  },
+  {
+    path: "beer-details",
+    component: BeerDetailsComponent
   }
 ];
