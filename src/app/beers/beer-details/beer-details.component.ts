e311575d1dbb0367305cee59721619a6ddf225b1
John <johnnymoran44@hotmail.com>
2019-07-18 15:10:35 +0200

    beers showing up with ngIf, styled well

--- src/app/beers/beer-details/beer-details.component.ts
+++ src/app/beers/beer-details/beer-details.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { select, Store } from "@ngrx/store";
+import { DrinksState } from "../store";
+import { Observable } from "rxjs/index";
+import { getBeersSelector } from "../store/beers.selectors";
+import { fetchBeersListRequest } from "../store/beers.actions";
 
 @Component({
-  selector: 'app-beer-details',
-  templateUrl: './beer-details.component.html',
-  styleUrls: ['./beer-details.component.scss']
+  selector: "app-beer-details",
+  templateUrl: "./beer-details.component.html",
+  styleUrls: ["./beer-details.component.scss"]
 })
 export class BeerDetailsComponent implements OnInit {
+  public beers$: Observable<any>;
+  id: number;
 
-  constructor() { }
+  constructor(private store: Store<DrinksState>, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.store.dispatch(fetchBeersListRequest());
+    this.beers$ = this.store.pipe(select(getBeersSelector));
+    this.getId();
   }
 
+  getId(): void {
+    this.id = +this.route.snapshot.paramMap.get("id");
+    this.id = this.id - 1;
+    console.log("id: " + this.id);
+  }
 }
