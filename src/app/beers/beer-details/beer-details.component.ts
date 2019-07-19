1710c44b96b85077cf91b51d74360de6f44138a5
John <johnnymoran44@hotmail.com>
2019-07-19 11:07:31 +0200

    add search

--- src/app/beers/beer-details/beer-details.component.ts
+++ src/app/beers/beer-details/beer-details.component.ts
@@ -15,7 +15,10 @@ import { Location } from "@angular/common";
 export class BeerDetailsComponent implements OnInit {
   public beers$: Observable<any>;
   id: number;
-  twitter: string;
+  target_og: number;
+  target_fg: number;
+  gravityPercent: number;
+  private show: boolean = false;
 
   constructor(private store: Store<DrinksState>, private route: ActivatedRoute, private location: Location) {}
 
@@ -29,6 +32,24 @@ export class BeerDetailsComponent implements OnInit {
     this.id = +this.route.snapshot.paramMap.get("id");
     this.id = this.id - 1;
     console.log("id: " + this.id);
+  }
+
+  getGravity() {
+    this.beers$.subscribe(res =>
+      res.map((beer, index) => {
+        if (index === this.id) {
+          this.target_og = beer.target_og;
+          this.target_fg = beer.target_fg;
+          let decrease = this.target_og - this.target_fg;
+          this.gravityPercent = (decrease / this.target_og) * 100;
+        }
+      })
+    );
+  }
+
+  toggle() {
+    this.show = !this.show;
+    this.getGravity();
   }
 
   goBack(): void {
